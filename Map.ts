8ee763c1a3cf7bb305f3f1db49cbac588f642a36
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-07-04 00:16:15 -0300

    Create map.ts

---
+++ Map.ts
@@ -0,0 +1,21 @@
+const arr1 = [1, 2, 3]
+const arr2 = ['1', '2', '3']
+
+const fn = <T>(x: T): Array<T> => [x, x]
+
+const fn2 = <T>(x: Array<T>): { key: T; value: T } => ({
+	key: x[0],
+	value: x[1]
+})
+
+type MapLike = Array<Record<number, number>>
+
+const arr3 = arr1.map(fn)
+
+// const map1: Map<number, number> = new Map(arr3)  BREAKS :/
+
+const map1: Map<number, number> = new Map([[1,1],[2,2],[3,3]])
+
+console.time('t')
+const val1 = map1.get(1)
+console.timeEnd('t')
