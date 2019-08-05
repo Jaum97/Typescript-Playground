27003639d9ec2751c9595cf464510fd4d0591aef
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-08-04 22:49:17 -0300

    create partial.ts

---
+++ partial.ts
@@ -0,0 +1,19 @@
+//https://www.typescriptlang.org/docs/handbook/utility-types.html#partialt
+
+interface Todo {
+    title: string;
+    description: string;
+}
+
+function updateTodo(todo: Todo, fieldsToUpdate: Partial<Todo>) {
+    return { ...todo, ...fieldsToUpdate };
+}
+
+const todo1 = {
+    title: 'organize desk',
+    description: 'clear clutter',
+};
+
+const todo2 = updateTodo(todo1, {
+    description: 'throw out trash',
+});
