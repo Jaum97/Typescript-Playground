094a7e80cc2cffc167ce87ddf64047857ff0e3d3
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-08-08 22:02:27 -0300

    Update

--- omitKeys.ts
+++ omitKeys.ts
@@ -1,24 +1,20 @@
-export const omitKeys = <T, K extends keyof T>(
-	obj: T,
-	keysToOmit: Array<K>
-): Pick<T, Exclude<keyof T, K>> => {
-	if (Object.keys(obj).length) {
-		const wantedKeys = Object.keys(obj).filter(key =>
-			keysToOmit.every(omit => omit !== key)
-		)
+export const omitKeys = < T,
+    K extends keyof T > (
+        obj: T,
+        keysToOmit: Array < K >
+    ): Omit < T, K > => {
+        if (Object.keys(obj).length) {
+            const wantedKeys = Object.keys(obj).filter(key =>
+                keysToOmit.every(omit => omit !== key)
+            )
 
-		const wantedObj = wantedKeys.map(key => [key, obj[key]])
+            const resultObj = wantedKeys.reduce((resultObj, key) => Object
+                .assign(resultObj, {
+                    [key]: obj[key]
+                }), ({}
+                    as unknown) as T)
 
-		const result = wantedObj.reduce(
-			(resultObject, keyValuePair) => {
-				resultObject[keyValuePair[0]] = keyValuePair[1]
-
-				return resultObject
-			},
-			({} as unknown) as T
-		)
-
-		return result
-	}
-	return obj
-}
+            return resultObj
+        }
+        return obj
+    }
