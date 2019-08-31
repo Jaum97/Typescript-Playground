1bbea48ff61fd6f34cfd9dbf1ab257d5c7939092
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-08-31 17:23:55 -0300

    update

--- pickProp.ts
+++ pickProp.ts
@@ -1,22 +1,31 @@
 //https://pt.stackoverflow.com/questions/406615/typescript-error-type-k-cannot-be-used-to-index-type-t
 //thanks to user140828 for helping me type this function!
 
-function pickProp<T extends string | number | symbol>(prop: T) {
-    return function<V extends {[key in T]: any}>(obj: V) {
-        return obj[prop]
-    }
-}
+type validProp = string | number | symbol
+
+type pickPropType = <T extends validProp>(prop: T) => <V extends {[key in T]: any}>(obj: V) => V[T]
+
+/**
+ * pickProp - HOF that receives prop then returns a function 
+ * @param prop property to be picked
+ * @return function that will pick that prop from the passed object
+ * 
+ * @param obj object from which the prop will be picked
+ * @return property prop from obj
+ */
+
+const pickProp: pickPropType = prop => obj => obj[prop]
 
 const pickName = pickProp('name')
 
 const obj = {
-	brand: '',
-	age: 21
+  value: 300,
+  brand: '123',
 }
 
-const t = pickName(obj)
+const label = pickName(obj)
 
-/*
-Argument of type '{ brand: string; age: number; }' is not assignable to parameter of type '{ name: any; }'.
-  Property 'name' is missing in type '{ brand: string; age: number; }' but required in type '{ name: any; }'.
-*/
+/**
+ * Argument of type '{ value: number; brand: string; }' is not assignable to parameter of type '{ name: any; }'.
+  Property 'name' is missing in type '{ value: number; brand: string; }' but required in type '{ name: any; }'.
+ */
