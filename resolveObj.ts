dd834c881e90a4610d98a4acd1ef5acbbaf3d3c0
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-09-04 14:26:39 -0300

    update jsdoc

--- resolveObj.ts
+++ resolveObj.ts
@@ -27,3 +27,30 @@ const resolveObject = async function <T>(obj: T): Promise<T>{
 }
 
 const obj2 = resolveObject(obj).then(x => console.log({x}))
+
+
+
+------
+
+/**
+ * isPromise - check if x is Promise
+ * @param x can be anything
+ * @returns boolean
+ */
+export const isPromise = (x: any): boolean => x instanceof Promise
+
+/**
+ * resolveObject
+ * @param obj object with props that are promises
+ * @returns promise that resolves into object with all props resolved
+ */
+export const resolveObject = async function<T>(obj: T): Promise<T> {
+	const values = await Promise.all(Object.values(obj))
+
+	const res = Object.keys(obj).reduce(
+		(obj, key, i) => Object.assign(obj, { [key]: values[i] }),
+		{},
+	)
+
+	return res as T
+}
