c5387eb34b99167036a0c4188e594c1dbff6bf99
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-09-05 23:53:55 -0300

    Update

--- pickKeys.ts
+++ pickKeys.ts
@@ -1,3 +1,5 @@
+
+
 export const pickKeys = < T,
     K extends keyof T > (obj: T, keysToPick: Array < K > ): Pick < T, K > => {
         if (Object.keys(obj).length) {
@@ -14,8 +16,28 @@ export const pickKeys = < T,
         }
         return obj
     }
-type ShortPick = <T, K extends keyof T>(x:T, y: Array<K>) => Pick<T,K> | unknown  
 
-const shortPick: ShortPick = (x, y) => y.reduce((a, b) => Object.assign(a, {
+/**
+ * shortPick - shortVersion of @pickProps 
+ * @param x object to pick keys from
+ * @param y array of keys to pick from @x
+ */
+type ShortPickType = <T, K extends keyof T>(x:T, y: Array<K>) => Pick<T,K>  
+
+const shortPick: ShortPickType = (x, y) => y.reduce((a, b) => Object.assign(a, {
     [b]: x[b]
-}), {})
+}), {} as T)
+
+const obj = {
+  name: 'dave',
+  age: 21
+}
+
+const obj2 = shortPick(obj, ['job'])
+//Type 'string' is not assignable to type '"name" | "age"'.ts(2322)
+
+const obj3 = shortPick(obj, ['name'])
+//const obj3: Pick<{
+//   name: string;
+//   age: number;
+// }, "name">
