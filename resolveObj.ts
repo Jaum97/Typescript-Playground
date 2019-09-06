ac6f6a113f978fa3b79fae1ae0776c5da4d21339
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-09-06 00:16:23 -0300

    update

--- resolveObj.ts
+++ resolveObj.ts
@@ -40,17 +40,34 @@ const obj2 = resolveObject(obj).then(x => console.log({x}))
 export const isPromise = (x: any): boolean => x instanceof Promise
 
 /**
- * resolveObject
+ * resolveObject - takes object with props that are promises and return
+ * promise that resolves into object with all resolved props
  * @param obj object with props that are promises
  * @returns promise that resolves into object with all props resolved
  */
-export const resolveObject = async function<T>(obj: T): Promise<T> {
+
+type validProp = string | number | symbol
+
+interface ObjectWithPromise {
+	[x: validProp]: Promise<any>
+}
+
+// trying to enforce obj to have some prop that is a Promise
+
+export const resolveObject = async function<T extends ObjectWithPromise>(
+	obj: T
+): Promise<T> {
 	const values = await Promise.all(Object.values(obj))
 
-	const res = Object.keys(obj).reduce(
-		(obj, key, i) => Object.assign(obj, { [key]: values[i] }),
-		{},
-	)
+	const objectReducer = (obj, k, i) => Object.assign(obj, { [k]: values[i] })
+
+	const res = Object.keys(obj).reduce(objectReducer, {})
 
 	return res as T
 }
+
+const p1 = async <T>(x: T): Promise<T> => x
+
+const obj = { otherProp: '' }
+
+const obj2 = resolveObject(obj)
