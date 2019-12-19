754f96b223537be5179d496e5bb79080ed566695
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-12-19 17:24:05 -0300

    Create mongoHelpers.ts

---
+++ mongoHelpers.ts
@@ -0,0 +1,38 @@
+import { Schema, Model, DocumentQuery } from 'mongoose'
+import {
+	ValidMongoCollection,
+	ValidMongoId,
+	ValidMongoDocument,
+} from '../interfaces/mongo'
+
+export const createRefObject = (
+	collection: ValidMongoCollection,
+	required = false,
+) => ({
+	type: Schema.Types.ObjectId,
+	ref: collection,
+	required,
+})
+
+export function getMongoDocumentById<T extends ValidMongoDocument>(
+	collection: Model<T, {}>,
+): (_id: ValidMongoId) => DocumentQuery<T, T, {}> {
+	return function(_id: ValidMongoId): DocumentQuery<T, T, {}> {
+		if (!_id) {
+			throw {
+				code: 420,
+				message: 'O id é obrigatório',
+			}
+		}
+
+		return collection.findById(_id)
+	}
+}
+
+export function getMongoDocuments<T extends ValidMongoDocument>(
+	collection: Model<T, {}>,
+): () => DocumentQuery<Array<T>, T, {}> {
+	return function(): DocumentQuery<Array<T>, T, {}> {
+		return collection.find()
+	}
+}
