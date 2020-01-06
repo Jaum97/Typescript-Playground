4c40b0c2ff196994c89282aca7d27868bfa5867b
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2020-01-06 06:09:20 -0300

    Create iterators.ts

---
+++ iterators.ts
@@ -0,0 +1,11 @@
+interface Iterator<T> {
+  next(value?: any): IteratorResult<T>;
+  return?(value?: any): IteratorResult<T>;
+  throw?(e?: any): IteratorResult<T>;
+}
+
+interface AsyncIterator<T> {
+  next(value?: any): Promise<IteratorResult<T>>;
+  return?(value?: any): Promise<IteratorResult<T>>;
+  throw?(e?: any): Promise<IteratorResult<T>>;
+}
