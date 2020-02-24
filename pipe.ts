b458abbcb17d678a2dc6205a7dc11bcdd3eaf185
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2020-02-23 23:32:10 -0300

    Update pipe.ts

--- pipe.ts
+++ pipe.ts
@@ -10,7 +10,6 @@ export declare function pipe<F extends GenericFn[], R>(
 // the return type of Head<F> will be passed to
 // Head<Tail<F>> until all fns have been consumed
 // the R
-  
 type GenericTuple = [any, ...any[]]
 
 type Head<T extends any[]> = T extends GenericTuple ? T[0] : never
@@ -19,9 +18,20 @@ type Tail<T extends any[]> = ((...t: T) => any) extends ((_: any, ...tail: infer
 
 type HasTail<T extends any[]> = T extends ([] | [any]) ? false : true
 
-type Pipe<F extends GenericFn[]> = (f: F) => HasTail<F> extends true 
+type Pipe<F extends GenericFn[]> = <T>(f: Head<F>) => HasTail<F> extends true 
 	? Pipe<Head<Tail<F>>>
 	: ReturnType<Head<F>>
+
+type Pipe2<F extends GenericFn[]> = {
+	0: Pipe2<Tail<F>>
+	1: ReturnType<Head<F>>
+}[
+	HasTail<F> extends true 
+	? 0
+	: 1
+]
+
+
 
 const sum = (a: number,b: number): number => a + b
 
@@ -29,7 +39,7 @@ const toString = (a: number, b: number): string => String(a + b)
 
 const toArray = (a: string): Array<string> => [a]
 
-type test01 = Pipe<[typeof sum, typeof toString, typeof toArray]>
+type test01 = Pipe2<[typeof sum, typeof toString, typeof toArray]>
 
 export declare function pipe(...functions) {
 	return value => {
