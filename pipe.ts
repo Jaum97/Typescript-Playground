76ec4738ccedb8228fe4fa4e56cbb6fa2a007570
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2020-02-24 00:02:00 -0300

    Update pipe.ts

--- pipe.ts
+++ pipe.ts
@@ -22,24 +22,42 @@ type Pipe<F extends GenericFn[]> = <T>(f: Head<F>) => HasTail<F> extends true
 	? Pipe<Head<Tail<F>>>
 	: ReturnType<Head<F>>
 
+type Pipe3<F extends GenericFn[]> = {
+	0: Pipe3<Head<Tail<F>>>
+	1: never
+	2: ReturnType<Head<F>>
+}[
+	HasTail<F> extends true
+	? ReturnType<Head<F>> extends Parameters<Head<Tail<F>>>
+		? 0
+		: 2
+	: 2
+]
+
 type Pipe2<F extends GenericFn[]> = {
 	0: Pipe2<Tail<F>>
-	1: ReturnType<Head<F>>
+	1: Head<F>
 }[
 	HasTail<F> extends true 
 	? 0
 	: 1
 ]
 
+type test02<F extends GenericFn[]> = [ReturnType<Head<F>>] extends Parameters<Head<Tail<F>>> ? true : false
 
+type test04 = ReturnType<Head<[typeof sum, typeof toString, typeof toArray]>>
 
-const sum = (a: number,b: number): number => a + b
+type test05 = Parameters<Head<Tail<[typeof sum, typeof toString, typeof toArray]>>>
 
-const toString = (a: number, b: number): string => String(a + b)
+type test03 = test02<[typeof sum, typeof toArray]>
+
+const sum = (a: number,b: number = 0): number => a + b
+
+const toString = (a: number): string => String(a)
 
 const toArray = (a: string): Array<string> => [a]
 
-type test01 = Pipe2<[typeof sum, typeof toString, typeof toArray]>
+type test01 = Pipe3<[typeof sum, typeof toString, typeof toArray]>
 
 export declare function pipe(...functions) {
 	return value => {
