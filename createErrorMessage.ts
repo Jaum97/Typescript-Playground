95c6024ab0cf50c96f6c69048e6768a04a5278fa
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2020-03-10 16:28:07 -0300

    Create createErrorMessage.ts

---
+++ createErrorMessage.ts
@@ -0,0 +1,55 @@
+export function replaceInTemplate(template: string, value: string, matcher = '{PARAM}'): string {
+	return String(template).replace(matcher, value)
+}
+
+interface IParamsToValidate {
+	[name: string]: any	
+}
+
+/**
+ * getMissingDataMessage
+ * @param paramsToValidate object with the parameters to validate in the format 
+ * { nameToPrint: valueToValidate }
+ * @param baseMessage message to use as base of the template
+ * @example 
+ 	getMissingDataMessage({
+		nome: 'joao',
+		potato: 1,
+		skills: [],
+		'Cidade enviada': {},
+		banana: '',
+		potat1: undefined
+	}, 'getCities {PARAM} faltando')
+	// returns 'getCities skills, Cidade enviada, banana e potat1 faltando'
+
+	* @example
+	getMissingDataMessage({
+		skills: [],
+		banana: '',	
+	})
+	// returns 'skills e banana faltando no corpo da requisiçāo'
+ */
+export function getMissingDataMessage(
+	paramsToValidate: IParamsToValidate, 
+	baseMessage: string = '{PARAM} faltando no corpo da requisiçāo'
+): string {
+	if(!paramsToValidate || !Object.keys(paramsToValidate).length) {
+		return
+	}
+
+	const missing = Object.entries(paramsToValidate).map(([name, value]) => {
+		if(
+			Array.isArray(value) && !value.length 
+			|| isValidObj(value) && isObjEmpty(value) 
+			|| !value
+		) {
+			return name
+		}
+	}).filter(Boolean)
+
+	const len = missing.length
+
+	const formattedParams = `${missing.slice(0, len - 1).join(', ')} e ${missing[len - 1]}`
+
+	return missing.length ? replaceInTemplate(baseMessage, formattedParams) : ''
+}
