f200be5bbb310fbad0037dd896634d66f7cb4218
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2020-05-26 23:07:23 -0300

    Create record.ts

---
+++ record.ts
@@ -0,0 +1,11 @@
+interface PageInfo {
+  title: string;
+}
+
+type Page = "home" | "about" | "contact";
+
+const x: Record<Page, PageInfo> = {
+  about: { title: "about" },
+  contact: { title: "contact" },
+  home: { title: "home" }
+};
