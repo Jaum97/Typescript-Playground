dad77a1f93b3811cdb409a48460bfc344f21072b
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2020-06-25 18:31:51 -0300

    Update omitTypenameRecursive.ts

--- omitTypenameRecursive.ts
+++ omitTypenameRecursive.ts
@@ -3,34 +3,86 @@ export const omitKeys = <K extends string | number | symbol>(keys: Array<K>) => 
 >(
 	obj: O
 ): Omit<O, K> => {
+	if (typeof obj !== 'object' || obj === null) return obj
+
 	const created = {} as O
 
 	const objKeys = Object.keys(obj)
 
 	for (const key of objKeys) {
 		if (!keys.includes(key as any)) {
-			; (created as any)[key] = (obj as any)[key]
+			;(created as any)[key] = (obj as any)[key]
 		}
 	}
 
 	return created
 }
 
-const omitTypename = omitKeys(['__typename'])
+export const omitTypename = omitKeys(['__typename'])
 
-function omitTypenameRecursive(obj) {
-	const created = {}
+export function omitTypenameRecursive<T>(obj: T): T {
+	if (typeof obj !== 'object' || obj === null) return obj
+
+	const created = {} as any
 
 	for (var k in obj) {
 		const val = obj[k]
 
-		if (typeof val == "object" && val !== null)
-			created[k] = omitTypenameRecursive(omitTypename(val));
-		else
+		if (typeof val == 'object' && val !== null && !Array.isArray(val)) {
+			created[k] = omitTypenameRecursive(omitTypename(val))
+		} else if (Array.isArray(val)) {
+			created[k] = val.map(omitTypenameRecursive)
+		} else if (k !== '__typename') {
 			created[k] = val
+		}
 	}
 
-	return created
+	return created as T
 }
 
+const obj = {
+	__typename: 'a',
+	locations: [
+		{
+			__typename: 'a',
+			coordinates: [0,0],
+			radius: 0
+		},
+		{
+			__typename: 'a',
+			coordinates: [0,0],
+			radius: 0
+		},
+	],
+	a: {
+		__typename: 'a',
+		b: {
+			__typename: 'a',
+			c: {
+				__typename: 'a',
+				medias: [
+					{
+						__typename: 'a',
+						coordinates: [0,0],
+						radius: 0
+					},
+					{
+						__typename: 'a',
+						coordinates: [0,0],
+						radius: 0
+					},
+				]
+			}
+		}
+	}
+}
 
+const t00 = omitTypenameRecursive(obj).a.b.c.medias
+
+t00
+/*
+[ 
+	{ coordinates: [ 0, 0 ], radius: 0 }, 
+  	{ coordinates: [ 0, 0 ], radius: 0 } 
+] 
+*/
